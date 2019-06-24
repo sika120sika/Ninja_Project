176d14aaabfe4fd60387313b9e837b3f13532fe9
koppei-push <koppei.itudemo.max@gmail.com>
2019-06-24 10:10:19 +0900

    new file

---
+++ Player/Assets/CameraControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+    [SerializeField] float RotateSpeed;
+    [SerializeField] private float posY = 7.0f;
+    [SerializeField] private float posZ = 12.0f;
+
+    [SerializeField] private float LookY = 5.0f;
+
+    public float yaw, pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RotateSpeed = 1;
+
+        //カーソルを画面中央に固定
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {                      
+        //回転処理
+        yaw += Input.GetAxis("Mouse X") * RotateSpeed;               //横回転入力
+        pitch -= Input.GetAxis("Mouse Y") * RotateSpeed;             //縦回転入力
+
+        pitch = Mathf.Clamp(pitch, -80, 60);                        //縦回転角度制限する
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);      //回転の実行
+
+        //移動処理
+        Vector3 vec = -transform.forward;
+
+        transform.position = vec * posZ;
+
+        //プレイヤー位置を追従する
+        transform.position = new Vector3(
+            transform.position.x + Player.position.x, 
+            transform.position.y + Player.position.y + posY,
+            transform.position.z + Player.position.z        
+        );
+
+        Vector3 cleatePos = new Vector3(
+            Player.position.x,
+            Player.position.y + LookY,
+            Player.position.z
+            );
+
+        //プレイヤーを見る
+        transform.LookAt(cleatePos, Vector3.up);
+    }
+}
---
+++ Player/Assets/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PlayerState
+{
+    //立つ
+    Stand,
+
+    //歩く
+    Walk,
+
+    //走る
+    Run,
+
+    //しゃがむ
+    Squat,
+
+    //しゃがんで歩く
+    Squat_Run,
+
+    //攻撃
+    Attack
+}
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller = null;
+    private Animator anim = null;
+    private GameObject camera = null;
+
+    bool Leyflg = false;
+
+    //ヒットポイント
+    [SerializeField] private int HP = 100;
+
+    //歩くスピード
+    [SerializeField] private float WalkSpeed = 0.5f;
+
+    //振り向く速度
+    [SerializeField] private float applySpeed = 0.2f;
+
+    //重力
+    [SerializeField] private float Gravity = 0.5f;   
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        camera = Camera.main.gameObject;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vMove = new Vector3(0, 0, 0);
+
+        //移動ベクトルを設定
+        var vAddF = camera.transform.forward;
+        var vAddS = camera.transform.right;
+
+        //Y軸を０にして、正規化する
+        vAddF.y = 0;
+        vAddF.Normalize();
+        vAddS.y = 0;
+        vAddS.Normalize();
+
+        //移動ベクトル取得
+        if (Input.GetKey(KeyCode.W)) { vMove += vAddF; }
+        if (Input.GetKey(KeyCode.S)) { vMove += -vAddF; }
+        if (Input.GetKey(KeyCode.A)) { vMove += -vAddS; }
+        if (Input.GetKey(KeyCode.D)) { vMove += vAddS; }
+
+        //右クリック
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }        
+
+        // 速度調整
+        vMove.Normalize();
+        vMove *= WalkSpeed;
+
+
+        //キャラの振り向き処理
+        if (vMove.magnitude > 0)
+        {
+            //プレイヤーが進行方向に向く
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(vMove),
+            applySpeed
+            );
+
+            var tmpvec = new Vector3(vMove.x, 0, vMove.z);
+
+        }
+
+        //地面についていなっかたら落下
+        if (!characterController.isGrounded)
+        {
+            vMove.y -= Gravity;          
+        }        
+
+        
+
+        //移動を実行
+        controller.Move(vMove);
+    }
+}
